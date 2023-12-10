dec587d631206df204d1e7821281a7bcab8061f3
Admin <hassaanazeem9@gamil.com>
2023-12-10 19:21:24 +0500

    My first Commit of Task1

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,28 @@
+package org.example;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Client {
+    //    private static final String QUEUE_NAME = "myQueue";S
+    private static final String msg = "Hello from India";
+    private static final String exchangeName = "task_1_exchange";
+    private static final String routingKey = "task_routing_key";
+
+    public static void main(String[] args) {
+        try {
+            // Establish connection
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.exchangeDeclare(exchangeName, "direct");
+            channel.basicPublish(exchangeName, routingKey, null, msg.getBytes());
+            System.out.println("Message sent: " + msg);
+            channel.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,37 @@
+package org.example;
+import java.io.IOException;
+
+import com.rabbitmq.client.*;
+
+public class Server {
+    private static final String queue = "task1_queue";
+    private static final String exchangeName = "task_1_exchange";
+    private static final String routingKey = "task_routing_key";
+
+    public static void main(String[] args) {
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            // Declare exchange
+            channel.exchangeDeclare(exchangeName, "direct");
+
+            // Declare queue and bind it to the exchange
+            channel.queueDeclare(queue, false, false, false, null);
+            channel.queueBind(queue, exchangeName, routingKey);
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println("Received message: " + message);
+            };
+
+            channel.basicConsume(queue, true, deliverCallback, consumerTag -> { });
+            // Close connections
+            channel.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
