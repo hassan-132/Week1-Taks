0e6f88adb67a9508d0ffa2313ecaff37aa5891ad
hassan-132 <84912922+hassan-132@users.noreply.github.com>
2023-12-11 16:26:04 +0500

    Update Client.java

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -11,7 +11,6 @@ public class Client {
     public static void main(String[] args) {
         try {
             // Establish connection
-            System.out.println("1st Message from Client");
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost("localhost");
             Connection connection = factory.newConnection();
