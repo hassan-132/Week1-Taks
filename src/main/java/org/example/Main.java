4ed9e060ec13f35252bbc5adb5be942f88315a01
hassan-132 <84912922+hassan-132@users.noreply.github.com>
2023-12-11 14:14:12 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // comments
         System.out.println("Salam Pakistan!");
         System.out.println("Namaste India!");
     }
